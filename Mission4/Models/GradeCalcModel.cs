ddb55ba84107a6bef18a5f8280d03d1357dfe079
jacantsen <jacob.ac@sbcglobal.net>
2023-02-06 09:43:33 -0700

    Added new branch

---
+++ Mission4/Models/GradeCalcModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Mission4.Models
+{
+    public class GradeCalcModel
+    {
+        [Required]
+        [MaxLength(100)]
+        [MinLength(0)]
+        public decimal Assignments { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [MinLength(0)]
+        public decimal groupProject { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [MinLength(0)]
+        public decimal Quizzes { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [MinLength(0)]
+        public decimal Midterm { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [MinLength(0)]
+        public decimal Final { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [MinLength(0)]
+        public decimal Intex { get; set; }
+    }
+}
